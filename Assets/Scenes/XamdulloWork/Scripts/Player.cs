3f8d2ae47b1f6ece6d372e2d7c5a665b476d3cb7
HunterZwer <117033000+HunterZwer@users.noreply.github.com>
2024-11-02 21:37:36 +0500

    Health System, Red death platform
    Пока жизни это цифра от которой отнимается -1. Игрок подпрыгивает и мерцает как в аркадных играх

--- Assets/Scenes/XamdulloWork/Scripts/Player.cs
+++ Assets/Scenes/XamdulloWork/Scripts/Player.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
-
+using System.Collections;
 public class SimpleCharacterMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 1f;
     private Rigidbody rb;
+    [SerializeField] private int _lives = 3;
+    private Renderer _mesh;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        _mesh = GetComponent<Renderer>();
     }
 
     void Update()
@@ -30,5 +33,39 @@ public class SimpleCharacterMovement : MonoBehaviour
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+    }
+
+    public void Damage()
+    {
+        _lives -= 1;
+        Debug.Log("Player damage");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Red_Platform"))
+        {
+            Damage();
+            StartCoroutine(Wait());
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            StartCoroutine(ToggleMeshVisibility());
+        }
+    }
+
+    IEnumerator ToggleMeshVisibility()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            _mesh.enabled = false;
+            yield return new WaitForSeconds(0.1f);  // Adjust time as needed
+            _mesh.enabled = true;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(2);
+        // After 2 seconds, you could perform additional actions if needed, such as hiding the platform
     }
 }
