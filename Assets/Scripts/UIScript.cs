0c8bbbc1df9a34f02bd4ead69d4817995c56373c
HunterZwer <117033000+HunterZwer@users.noreply.github.com>
2024-11-03 19:06:51 +0500

    Timer & Lives in main scene

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 public class UIScript : MonoBehaviour
 {
     public static UIScript Instance;
     [SerializeField] private GameObject receiptPanel;
+    [SerializeField] private Text _livesText;
+    [SerializeField] private int _seconds;
+    [SerializeField] private Text _TimerSeconds;
+    [SerializeField] private GameObject losePanel;
 
     private void Awake()
     {
@@ -12,6 +18,9 @@ public class UIScript : MonoBehaviour
     private void Start()
     {
         receiptPanel.SetActive(true);
+        _livesText.text = "Lives " + 3;
+        _TimerSeconds.text = "Timer " + _seconds;
+        TimerCountDown();
     }
     public void PanelOpen(GameObject panel)
     {
@@ -20,9 +29,32 @@ public class UIScript : MonoBehaviour
     public void PanelClose(GameObject panel)
     {
         panel.SetActive(false);
-    }  
+    }
     public void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void UpdateLives(int playerHealth)
+    {
+        _livesText.text = "Lives " + playerHealth.ToString();
+    }
+
+
+    public void TimerCountDown()
+    {
+        StartCoroutine(TimerCounter());
+    }
+
+
+    IEnumerator TimerCounter()
+    {
+        while (_seconds > 0)
+        {
+            yield return new WaitForSeconds(1);
+            _seconds--;
+            _TimerSeconds.text = "Timer " + _seconds;
+        }
+        Debug.Log("Lose");
+        losePanel.SetActive(true);
     }
 }
